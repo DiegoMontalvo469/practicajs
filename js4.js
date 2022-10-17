be7011e85e1a51349846ade3f07ad5bbc5afd95f
DiegoMontalvo469 <dmontalvo469@gmail.com>
2022-10-17 09:21:47 -0500

    divisores ver 2

--- js4.js
+++ js4.js
@@ -10,3 +10,14 @@ function divisores(numero){
 }
 
 console.log("Total divisores", divisores(32));
+
+const divisoresV2 = function(numero){
+    let total = 0;
+
+    for(let i=1; i <=numero; i++){
+        if(numero % i == 0){
+            total++;
+        }
+    }
+    return total;
+};
