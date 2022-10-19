b65e094f4a98a9eaef9762ce804f2ee0e8e23e15
DiegoMontalvo469 <dmontalvo469@gmail.com>
2022-10-19 10:28:11 -0500

    js3

---
+++ js5.js
@@ -0,0 +1,15 @@
+function calcular(){
+    let numero = 
+    parseInt(document.getElementById("numero").value)
+    console.log(numero);
+    let resultado = 
+    document.getElementById("resultado");
+
+    resultado.value = numero;
+    resultado.value = divisores(numero);
+}
+
+function operation(op, n1, n2)
+    let res = 0
+        switch
+            case1
