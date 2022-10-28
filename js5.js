f008b984ae2c43e40f480bc97e90df487f1a962a
DiegoMontalvo469 <dmontalvo469@gmail.com>
2022-10-28 16:58:22 -0500

    js

--- js5.js
+++ js5.js
@@ -1,25 +1,24 @@
-function op(op, n1, n2) {
-    let res = 0
-        switch (numero) {
-            case "+":
-                res = n1 + n2
-                break;
-            case "-":
-                res = n1 - n2
-                break;
-            case "*":
-                res = n1 * n2
-                break;
-            case "/":
-                res = n1 / n2
-                break;
-            default:
-            break;}
+function operacion(op, n1, n2){
+    let res = 0;
+    switch (op){
+        case "+":
+            res = n1 + n2;
+            break;
+
+        case "-":
+            res = n1 - n2;
+            break;
+
+        case "*":
+            res = n1 * n2;
+            break;
+
+        case "/":
+            res = n1 / n2;
+            break;
     
+        default:
+            break;
     }
-
-    return res
-
-function calcular(){
-    let num1 = parseInt(document.getElementById)
+    return res;
 }
